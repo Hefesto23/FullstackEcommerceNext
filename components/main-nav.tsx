bc7dd309232e45e6345780c6052e60db80caf26b
Hefesto23 <vini.ra@outlook.com>
2023-09-13 16:36:29 -0300

    feat: Create Product and Colors Pages and Api

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -29,6 +29,16 @@ function MainNav({
       active: pathname === `/${params.storeId}/categories`,
     },
     {
+      href: `/${params.storeId}/products`,
+      label: 'Products',
+      active: pathname === `/${params.storeId}/products`,
+    },
+    {
+      href: `/${params.storeId}/colors`,
+      label: 'Colors',
+      active: pathname === `/${params.storeId}/colors`,
+    },
+    {
       href: `/${params.storeId}/sizes`,
       label: 'Sizes',
       active: pathname === `/${params.storeId}/sizes`,
