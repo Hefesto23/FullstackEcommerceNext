83f0075de169b3ff5ea9e8d744ba1c2c1ac4b0c8
Hefesto23 <vini.ra@outlook.com>
2023-09-13 18:04:51 -0300

    feat: Add Order Page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -34,6 +34,11 @@ function MainNav({
       active: pathname === `/${params.storeId}/products`,
     },
     {
+      href: `/${params.storeId}/orders`,
+      label: 'Orders',
+      active: pathname === `/${params.storeId}/orders`,
+    },
+    {
       href: `/${params.storeId}/colors`,
       label: 'Colors',
       active: pathname === `/${params.storeId}/colors`,
